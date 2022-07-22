3ea5f35d9add6827ceb7968c3c945588b5fcb5f9
laghig <lasker@gmx.ch>
2022-07-22 15:43:27 +0200

    Bag of words method

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,6 +7,8 @@ from nltk.stem.porter import * # porter stemmer was developed for the english la
 from nltk.stem.cistem import * # Cistem seems to be the best stemmer for the german language
 from nltk.stem.snowball import FrenchStemmer 
 
+# Own imports
+from bags_of_words import key_words, grouped_words
 # nltk.download('stopwords')
 """
 Cleaning:
@@ -18,6 +20,7 @@ Cleaning:
         no punctuation
         lowercase only
         remove stop words -> language dependency: en, de, fr
+        bag of words methodology --> language dependent
         stemming -> language dependency: en, de, fr
 """
 
@@ -26,20 +29,48 @@ Cleaning:
 def decimal_with_point(text):
     text = re.sub('(?<=\d),(?=\d)', '.', text)
     return text
+
 def remove_numbers(text):
     text = re.sub("\d+", "", text)
+    return text
+
+def replace_brackets(text):
+    text = str(text).replace("(", ", ").replace(")", " ,")
     return text
 
 def remove_punctuation(text):
     no_punct = text.translate(str.maketrans('', '', string.punctuation))
     return no_punct
 
+def remove_punct_subset(text):
+    to_delete = string.punctuation.replace(",", "").replace("()", "")
+    cleaned_text = text.translate(str.maketrans('', '', to_delete))
+    return cleaned_text
+
+def merge_ing(text):
+    text = re.sub("(?<=\w) +(?=\w+)", "", text)
+    return text
+
+def group_ing(text):
+    swapped_word_list = {
+        word: replacement
+        for replacement, words in grouped_words.items()
+        for word in words
+    }
+
+    new_text = ' '.join([
+            swapped_word_list.get(word, word)
+            for word in text.split()
+                ])
+    
+    return new_text
+
 def remove_stop_words_en(text):
     # Stop words - standard dictionary
     en_stopwords = stopwords.words('english')
 
     # Ingredients list- specific additions
-    en_stopwords.extend(['ingredients'])
+    en_stopwords.extend(['ingredients', 'emulsifiers'])
 
     filtered_words = [word for word in text if word not in en_stopwords]
 
@@ -48,13 +79,16 @@ def remove_stop_words_en(text):
 def remove_stop_words_de(text):
     # Stop words - standard dictionary
     de_stopwords = stopwords.words('german')
-
-    # Ingredients list- specific
-    de_stopwords.extend(['zutaten', 'bezeichnung', 'herkunft', 'gefangen', 'kann', 'enthalten', 'spuren'])
-
+    de_stopwords.extend(['zutaten', 'natürliches', 'schweiz', 'bio'])   
     filtered_words = [word for word in text if word not in de_stopwords]
+    ing_list = ' '.join([str(elem) for elem in filtered_words])
+    return ing_list
 
-    return filtered_words
+def bag_of_words_de(text):
+
+    filtered_words = [word for word in text if word in key_words]
+    ing_list = ' '.join([str(elem) for elem in filtered_words])
+    return ing_list
 
 def remove_stop_words_fr(text):
     # Stop words - standard dictionary
@@ -101,7 +135,13 @@ def clean_dataframe(df, language):
     if language == 'fr':
         df = df[df.text != 'Produkt'] # add here all prod_id with the text in german
 
-    return df
+    if language == 'de':
+        df = df[df.text != 'Produkt']
+        df = df[df.text != 'Keine Zutatenliste']
+        df = df[df.text != 'Keine Zutatenliste.'] 
+        df = df[df.text != 'KEINE ZUTATENLISTE.']
+        df = df[df.text != 'Keine.']
+        return df
 
 def clean_OFF_dataframe(df, language):
     # Drop empty values
@@ -117,17 +157,19 @@ def first_five_ing(text):
     Input: string of the ingredients text separated by comma
     Output: string with only the fist five ingredients
     """
-    text = ",".join(text.split(",",5)[:-1])
+    text = ",".join(text.split(",",1)[:-1])
     return text
 
 def text_cleaning(df, language, column, model_modifications):
 
     if model_modifications['OnlyFive'] is True:
         df[column]= df[column].apply(lambda x: first_five_ing(x))
+
+    df[column] = df[column].apply(lambda x: replace_brackets(x))
     
     # remove punctuation
-    df[column] = df[column].apply(lambda x: remove_punctuation(x))
-
+    df[column] = df[column].apply(lambda x: remove_punct_subset(x))
+    
     # remove numbers
     df[column] = df[column].apply(lambda x: remove_numbers(x))
 
@@ -141,19 +183,29 @@ def text_cleaning(df, language, column, model_modifications):
         df[column]=df[column].apply(lambda x: word_stemmer_en(x))
     
     if language == 'de':
-        df[column]= df[column].apply(lambda x: remove_stop_words_de(x))
-        df[column]=df[column].apply(lambda x: word_stemmer_de(x))
+
+        # df[column]= df[column].apply(lambda x: remove_stop_words_de(x))
+        # df[column]=df[column].apply(lambda x: word_stemmer_de(x))
+
+        df[column]= df[column].apply(lambda x: bag_of_words_de(x))
+        # remove the remaining punctuation
+        df[column] = df[column].apply(lambda x: remove_punctuation(x))
+        df[column]=df[column].apply(lambda x: group_ing(x))
+         
     
     if language == 'fr':
         df[column]= df[column].apply(lambda x: remove_stop_words_fr(x))
         df[column]=df[column].apply(lambda x: word_stemmer_fr(x))
+
+    # Merge ingredients comoosed by multiple words
+    # df[column] = df[column].apply(lambda x: merge_ing(x))
 
     return df
 
 if __name__ == "__main__":
     
     # TEST
-    # random_text = "I love veggies, but I can't imagine to not eat meat - it's just too good! I: ?just try ; to eat^ less of it."
+    # random_text = " sugar, palm oil, hazelnuts 13%, skimmed milk powder 8,7%, lean cocoa is 7,4%, emulsifiers : lecithins [soy] , vanillin,"
 
     # clean_text = remove_punctuation(random_text)
     # print(clean_text)
@@ -164,11 +216,9 @@ if __name__ == "__main__":
     # clean_text=word_stemmer_en(clean_text)
     # print(clean_text)
 
-    # Model Modifications:
+    # Model/text Modifications:
+    # clean= decimal_with_point(rnd_text)
+    # print(first_five_ing(clean))
 
-    rnd_text = " sugar, palm oil, hazelnuts 13%, skimmed milk powder 8,7%, lean cocoa is 7,4%, emulsifiers : lecithins [soy] , vanillin,"
-    # Problem: in OFF database percentages are comma separated
-
-    clean= decimal_with_point(rnd_text)
-    print(clean)
-    #print(first_five_ing(clean))
+    # clean = group_ing('biovollmilch pasteurisier lacto schweiz bergmilch biorahm schweiz salz jodfluorfrei schweiz sauerung')
+    # print(clean)
